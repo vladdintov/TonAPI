69f5606514307664203ac97b9c8fd7ff169ff5ed
vladdintov <vladdintov@gmail.com>
2025-02-07 16:38:33 +0300

    Update main.py
    added stonfi api

--- tonapi/main.py
+++ tonapi/main.py
@@ -3,18 +3,20 @@ from typing import Any, Literal
 from aiohttp import ClientSession
 
 
-class TonAPI:
+class API:
     """API class."""
 
-    base_url = "https://tonapi.io/v2/rates?tokens={tokens}&currencies={currencies}"
+    base_tonapi_url = "https://tonapi.io/v2/rates?tokens={tokens}&currencies={currencies}"
+    stonfi = "https://api.ston.fi/v1/assets/"
 
-    async def get_rate(
+
+    async def get_rate_by_tonapi(
             self: Any,
             tokens: str,
             currencies: str,
             ) -> float:
 
-        url = self.base_url.format(
+        url = self.api.format(
             tokens=tokens,
             currencies=currencies,
             )
@@ -23,7 +25,7 @@ class TonAPI:
             response_data = await response.json()
             return response_data["rates"][tokens]["prices"][currencies]
 
-    async def get_diff(
+    async def get_diff_by_tonapi(
             self: Any,
             tokens: str,
             currencies: str,
@@ -39,3 +41,18 @@ class TonAPI:
             response = await session.get(url=url)
             response_data = await response.json()
             return response_data["rates"][tokens][f"diff_{diff}"][currencies]
+
+    async def get_rate_by_stonfi(
+            self: Any,
+            token: str,
+            ) -> float:
+
+        url = f"{self.stonfi}{token}"
+        async with ClientSession() as session:
+            response = await session.get(
+                url=url,
+                headers={
+                "Accept": "application/json",
+                })
+            data = await response.json()
+            return data["asset"]["dex_usd_price"]
